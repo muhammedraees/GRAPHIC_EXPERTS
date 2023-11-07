e6948c1ba4f1938a52f2648011a9df2ecb1b29b4
raees <muhammedraees911@gmail.com>
2023-11-07 19:02:25 +0530

    post with bugs

--- lib/client_post_new/post_listing_cli.dart
+++ lib/client_post_new/post_listing_cli.dart
@@ -1,7 +1,89 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'create_post_cli.dart';
+// import 'post_model_cli.dart'; // Import the Post class
+// // import 'create_post_page.dart'; // Import the CreatePostPage
 
+// class UserPostsPage extends StatefulWidget {
+//   @override
+//   _UserPostsPageState createState() => _UserPostsPageState();
+// }
+
+// class _UserPostsPageState extends State<UserPostsPage> {
+//   final User? user = FirebaseAuth.instance.currentUser;
+
+//   Future<List<Post>> _getUserPosts() async {
+//     final QuerySnapshot snapshot = await FirebaseFirestore.instance
+//         .collection('posts')
+//         .where('userId', isEqualTo: user?.uid)
+//         .get();
+//     return snapshot.docs.map((doc) {
+//       final data = doc.data() as Map<String, dynamic>;
+//       return Post(
+//         userId: data['userId'],
+//         image: data['image'],
+//         caption: data['caption'],
+//       );
+//     }).toList();
+//   }
+
+//   void _navigateToCreatePostPage(BuildContext context) {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => CreatePostPage(),
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Your Posts'),
+//       ),
+//       body: FutureBuilder<List<Post>>(
+//         future: _getUserPosts(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return CircularProgressIndicator();
+//           } else if (snapshot.hasError) {
+//             return Text('Error: ${snapshot.error}');
+//           } else {
+//             final List<Post> userPosts = snapshot.data!;
+//             return ListView.builder(
+//   itemCount: userPosts.length,
+//   itemBuilder: (context, index) {
+//     final post = userPosts[index];
+//     return ListTile(
+//       title: Text(post.caption),
+//       // Display the image here using Image.network
+//       leading: Image.network(post.image),
+//     );
+//   },
+// );
+
+//           }
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           _navigateToCreatePostPage(context);
+//         },
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'create_post_cli.dart';
 import 'post_model_cli.dart';
 
 class UserPostsPage extends StatefulWidget {
@@ -12,7 +94,6 @@ class UserPostsPage extends StatefulWidget {
 class _UserPostsPageState extends State<UserPostsPage> {
   final User? user = FirebaseAuth.instance.currentUser;
 
-  // Fetch posts created by the logged-in user
   Future<List<Post>> _getUserPosts() async {
     final QuerySnapshot snapshot = await FirebaseFirestore.instance
         .collection('posts')
@@ -23,9 +104,18 @@ class _UserPostsPageState extends State<UserPostsPage> {
       return Post(
         userId: data['userId'],
         image: data['image'],
-        caption: data['caption'],
+        caption: data['caption'], userEmail: '',
       );
     }).toList();
+  }
+
+  void _navigateToCreatePostPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CreatePostPage(),
+      ),
+    );
   }
 
   @override
@@ -49,12 +139,19 @@ class _UserPostsPageState extends State<UserPostsPage> {
                 final post = userPosts[index];
                 return ListTile(
                   title: Text(post.caption),
-                  // Display the image here
+                  // Display the image here using Image.network
+                  leading: Image.network(post.image),
                 );
               },
             );
           }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _navigateToCreatePostPage(context);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
--- lib/client_post_new/post_model_cli.dart
+++ lib/client_post_new/post_model_cli.dart
@@ -1,11 +1,26 @@
+// class Post {
+//   final String userId;
+//   final String image;
+//   final String caption;
+
+//   Post({
+//     required this.userId,
+//     required this.image,
+//     required this.caption,
+//   });
+// }
+
+
 class Post {
   final String userId;
   final String image;
   final String caption;
+  final String userEmail; // Add this property
 
   Post({
     required this.userId,
     required this.image,
     required this.caption,
+    required this.userEmail,
   });
 }
